56804d25aa08a72ecfebfdfe1889f0edbd436e65
Will Wu <williamwu8-8@outlook.com>
2022-01-16 17:01:28 -0600

    Added nitrogen and xbanish support

--- autostart.sh
+++ autostart.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
-xrdb ~/.Xresources
-xmodmap ~/.xmodmap
-emacs --daemon
+# xrdb ~/.Xresources
+xmodmap ~/.xmodmap #map caps to ctl
+emacs --daemon #star emacs background server
+nitrogen --restore # wallpaper setter
+xbanish &! # mouse inhibitor
 # trayer &!
 # nm-applet &!
 # redshift -l 38.6:90.3 -t 6500:3600 -g 0.8 -m randr -v &!
--- colors.py
+++ colors.py
@@ -1,6 +1,6 @@
-primaryColor = "0E86D4"
-secondaryColor = "F04854"
-tertiaryColor1 = "042540"
+primaryColor = "0B55A3"
+secondaryColor = "ADADAD"
+tertiaryColor1 = "041A33"
 tertiaryColor2 = "E2E4E7"
 
 wpl = '~/Pictures/Wallpapers/0076.jpg'
